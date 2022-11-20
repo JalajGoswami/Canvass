9e2eab5cdafbfe906fd45fd0646bef1c26958fe8
Jalaj Goswami <jalajgoswami33@gmail.com>
2022-11-20 14:53:33 +0530

    configured redux store

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-import {AppRegistry} from 'react-native';
+import 'react-native-gesture-handler';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { Provider } from 'react-redux'
+import store from './store/store'
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+    return (
+        <Provider store={store}>
+            <App />
+        </Provider>
+    );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
