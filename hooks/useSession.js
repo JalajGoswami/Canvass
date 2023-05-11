daf5bb215c721f4f9640c6ce1e05ae682463c050
Jalaj Goswami <jalajgoswami33@gmail.com>
2023-05-11 14:08:17 +0530

    create useSession custom hook & reducers in progress

---
+++ hooks/useSession.js
@@ -0,0 +1,69 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { login, logOut, userRestored, userRestoreFailed } from '../store/features/userProfile'
+import EncryptedStorage from 'react-native-encrypted-storage';
+
+const saveSession = async (user) => {
+    await EncryptedStorage.setItem('user', JSON.stringify(user))
+    await EncryptedStorage.setItem('token', user.accessToken)
+}
+
+const deleteSavedData = async () => {
+    await EncryptedStorage.clear()
+}
+
+export default function useSession() {
+    const user = useSelector(state => state.user)
+    const dispatch = useDispatch()
+
+    const signIn = (payload) => {
+        dispatch(login(payload))
+    }
+
+    const signOut = () => {
+        deleteSavedData()
+        dispatch(logOut())
+    }
+
+    return {
+        loading: user.loading,
+        isAuthorized: user.isAuthorized,
+        accessToken: user.accessToken,
+        signIn,
+        signOut,
+        error: user.error,
+        user: user.user,
+    }
+}
+
+export const useRootSession = () => {
+    const user = useSelector(state => state.user)
+    const dispatch = useDispatch()
+
+    const restoreSession = async () => {
+        try {
+            let userData = await EncryptedStorage.getItem('user')
+
+            if (!userData) {
+                dispatch(userRestoreFailed())
+                return
+            }
+
+            const userState = JSON.parse(user)
+            dispatch(userRestored(userState))
+        }
+        catch (err) { dispatch(userRestoreFailed()) }
+    }
+
+    useEffect(() => {
+        if (user.user)
+            saveSession(user)
+    }, [user.user])
+
+    const session = useSession()
+
+    return {
+        ...session,
+        restoreSession,
+    }
+}
--- store/slices/user.js
+++ store/slices/user.js
@@ -1,12 +1,16 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
     loading: false,
     isAuthorized: null,
     accessToken: null,
     error: null,
-    username: null,
+    user: null
 }
+
+const login = createAsyncThunk('user/login', async (body) => {
+    
+})
 
 const user = createSlice({
     name: 'user',
