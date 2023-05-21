c07aba86894046ac952c14a588a122af72f09c1f
Jalaj Goswami <jalajgoswami33@gmail.com>
2023-05-21 11:34:45 +0530

    configured splash screen for both dark & light theme

--- Screens/Common/Loading.js
+++ Screens/Common/Loading.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import StyledText from 'Components/Common/StyledText'
+import React, { useEffect } from 'react'
+// import StyledText from 'Components/Common/StyledText'
 import StyledBody from 'Components/Common/StyledBody'
+import RNBootSplash from 'react-native-bootsplash'
 
 export default function Loading() {
+  useEffect(() => () => {
+    RNBootSplash.hide({ fade: true })
+  }, [])
+
   return (
     <StyledBody>
-      <StyledText size={25}>Loading</StyledText>
+      {/* <StyledText size={25}>Loading</StyledText> */}
     </StyledBody>
   )
 }
